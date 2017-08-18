973ebeee640474031720858fb35131e9a04a3628
Mahendran <mahendran@mindmade.in>
2017-08-18 10:39:59 +0530

    Material Design customize library added

---
+++ MaterialDesign/src/main/java/mindmade/materiladesign/material_edit_text/validation/METLengthChecker.java
@@ -0,0 +1,10 @@
+package mindmade.materiladesign.material_edit_text.validation;
+
+/**
+ * Created by mariotaku on 15/4/12.
+ */
+public abstract class METLengthChecker {
+
+    public abstract int getLength(CharSequence text);
+
+}
